4c9dbf0725dcf390dbda153c7a7d8e5ec01a8fc6
Faiq1ktk <145838796+Faiq1ktk@users.noreply.github.com>
2026-06-18 16:42:30 +0500

    ok

--- PropsPrectice/src/App.jsx
+++ PropsPrectice/src/App.jsx
@@ -1,14 +1,29 @@
-
-import './App.css'
+import './components/card.css';
+import Card from './components/card';
+import './App.css';
 
 function App() {
-
-
   return (
     <>
-     <h1>Hello Props</h1>
+
+      <Card name="abdul Faiq">
+        <p>
+          Hello I am Abdul Faiq, I am a software developer. I have experience in web development and mobile app development. I am passionate about learning new technologies and improving my skills. I am also a team player and I enjoy working with others to achieve common goals.
+        </p>
+        <h1>
+          Hello Props
+        </h1>
+        <h2>okay</h2>
+
+      </Card>
+
+
+
+      <h1>Hello Props</h1>
+      
+      <h1>Hello Props</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
